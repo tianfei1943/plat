fd0e71c9c66d9f1680f5c85e4266ea6e80bcfdb8
tianfei1943 <041621219@163.com>
2016-02-16 09:38:28 +0800

    fffff

--- src/test/java/gson/TestJson.java
+++ src/test/java/gson/TestJson.java
@@ -3,6 +3,8 @@ package gson;
 import org.junit.Test;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 public class TestJson {
 
@@ -15,6 +17,21 @@ public class TestJson {
 		o.setAge(11);
 		o.setName("fei");
 		System.out.println(gson.toJson(o));
+		
+		
+		JsonParser jsonParser = new JsonParser();
+        JsonObject jsonObject = (JsonObject) jsonParser.parse("");
+        if(jsonObject.has("err")){
+        		//后台报错
+        }else{
+        		if(jsonObject.isJsonNull()){
+        			//正常
+        		}else{
+        			//空窜
+        		}
+        }
+		
+		
 	}
 	
 
