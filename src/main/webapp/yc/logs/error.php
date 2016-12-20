0290cfe94bf3de5ec98bd4a7dfecada5e6f41bc7
tianfei1943 <041621219@163.com>
2016-12-20 22:11:47 +0800

    99
    888

--- src/main/webapp/yc/logs/error.php
+++
@@ -1,9 +0,0 @@
-#
-#<?php die('Forbidden.'); ?>
-#Date: 2016-10-24 06:43:15 UTC
-#Software: Joomla Platform 11.4.0 Stable [ Brian Kernighan ] 03-Jan-2012 00:00 GMT
-
-#Fields: date	time	priority	clientip	category	message
-2016-10-24	06:43:15	INFO	127.0.0.1	Joomla FAILURE: 	Username and password do not match or you do not have an account yet.
-2016-10-26	05:40:30	INFO	127.0.0.1	Joomla FAILURE: 	用户名和密码不匹配或者您尚未注册一个帐号。
-2016-10-27	08:40:09	INFO	127.0.0.1	Joomla FAILURE: 	用户名和密码不匹配或者您尚未注册一个帐号。
